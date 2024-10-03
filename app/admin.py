25e78a40a4a6754be73341be99fb15374e484dcf
Joao Vitor <joaovrsilveira16@gmail.com>
2024-10-03 16:31:47 -0300

    Criação do admin e mais uma classe dentro de models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+#importanto Topic criado em models
+from app.models import Topic, Entry
+
+# registra no site na page do admin o topic criado em models
+admin.site.register(Topic)
+
+admin.site.register(Entry)
--- app/models.py
+++ app/models.py
@@ -5,6 +5,20 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-# retorna uma representacao em str do modelo
+# retorna uma representacao em str do modelo para ser possivel identificar o que é (remove a criptografia)
     def __str__(self):
         return self.text
+    
+# classe que cria a anotação de dentro do topico
+class Entry(models.Model):
+    # link essa entry com o topic atraves da foreignkey, on_delete é obrigatório passar
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # mudar o plural, sem isso o django apenas adiciona S no final da class transformando de entrys nesse caso
+    class Meta:
+        verbose_name_plural = 'entries'
+
+        def __str__(self):
+            return self.text[:50] + '...'
